d5fc627ce1ce8e940e7398070487b151952f3fb8
brodrickgeorge <brodrick.george@yahoo.com>
2020-10-27 23:53:48 -0500

    commit tree 3966b71c9d79c0992ad5c285beeabedf381285dd

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,17 @@
-# code here!
+
+require 'pry'
+
+class School
+    def initialize(name)
+    end
+       
+        def roster
+            roster = {}
+        end
+
+        def add_student(student_name, grade)
+            roster
+        end
+        binding.pry
+    end
+
